dc5bd4fd6f6e47b1dbdf96312c09b239573f4aa6
yohany_06 <yohsantanilla@misena.edu.co>
2023-09-30 23:15:41 -0500

    Diseño de footer

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="assets/css/footer.css" />
+    <link rel="stylesheet" href="assets/css/index.css" />
+    
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>Farmadso</title>
+</head>
+
+<body>
+    <?php 
+    require_once 'templates/header.html';
+    ?>
+
+    <?php 
+    require_once 'templates/footer.html';
+    ?>
+
+    <script src="assets/js/index.js"></script>
+</body>
+
+</html>
